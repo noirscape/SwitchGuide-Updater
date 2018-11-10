96c0378f809ba057b44d563de086b214e89d4a68
noirscape <deepnavy@waifu.club>
2018-11-10 21:53:38 +0100

    Mega-derp

--- switchguideupdater.py
+++ switchguideupdater.py
@@ -12,7 +12,7 @@ import traceback
 from enum import Enum
 
 BASE_URL = 'http://amsupdater.catgirlsin.space/json/'
-VERSION = '1.1.0'
+VERSION = '1.1.1'
 
 class WindowState(Enum):
     MAIN_MENU = 0
